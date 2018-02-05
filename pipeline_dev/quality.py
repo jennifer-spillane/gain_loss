dda12980d19635bb57b9f7ec8e87f58d31cdcf2d
Jennifer L Hill <jenniferlhill@Jennifers-MacBook-Air.local>
2018-02-05 14:39:34 -0500

    Updated line parsing

--- pipeline_dev/quality.py
+++ pipeline_dev/quality.py
@@ -22,17 +22,22 @@ def busco():
     #looping through the files in a specified directory
     for entry in os.scandir("{0}".format(args.path)):
         print(entry.path)
+        print(entry.name)
         #running busco with the metazoa database
         subprocess.run("run_BUSCO.py -i {0} -o {1}_qual -l {2}/metazoa_odb9 -m prot".format(entry.path, entry.name, orig_dir), shell = True)
 
         #opening the output files and finding the relevant score
         try:
-            with open("run_{0}_qual/short_summary_{0}_qual_out.txt".format(entry.path)) as qual_file:
+            with open("run_{0}_qual/short_summary_{0}_qual.txt".format(entry.name)) as qual_file:
                 score = 0
+                count = 0
                 for line in qual_file:
-                    line = line.strip()
-                    if line[0] == "C":
-                        score += int(line[2:6])
+                    count += 1
+                    line = line.rstrip()
+                    if count == 8:
+                        break
+                print(line)
+                score += int(line[2:6])
                 print(score)
                 #sorting the files based on the score
                 if score < 50.0:
@@ -46,8 +51,8 @@ def busco():
     os.chdir(orig_dir)
     os.rename("busco_out/config.ini", "{0}/config.ini".format(orig_dir))
 
-    parser = argparse.ArgumentParser(description = "Arguments for the final project pipeline")
-    parser.add_argument("path", help = "path to a directory containing protein datasets ready for busco")
-    args = parser.parse_args()
+parser = argparse.ArgumentParser(description = "Arguments for the final project pipeline")
+parser.add_argument("path", help = "path to a directory containing protein datasets ready for busco")
+args = parser.parse_args()
 
-    busco()
+busco()
