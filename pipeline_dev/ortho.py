ad8611273f42e158321a953f2b7978eaedbd9466
Jennifer L Hill <jenniferlhill@Jennifers-MacBook-Air.local>
2018-02-06 12:21:19 -0500

    Orthofinder updates

--- pipeline_dev/ortho.py
+++ pipeline_dev/ortho.py
@@ -11,9 +11,9 @@ def orthofinder():
     orig_dir = os.getcwd()
     os.mkdir("orthofinder_out")
     os.chdir("orthofinder_out")
-    trees = subprocess.run("orthofinder.py -f {0} -t 24".format(args.dir), shell = True, stdout = subprocess.PIPE)
+    subprocess.run("orthofinder.py -f {0} -t 24".format(args.dir), shell = True, stdout = subprocess.PIPE)
     os.chdir("{0}".format(orig_dir))
-    return trees.stdout.decode('ascii')
+    #return trees.stdout.decode('ascii')
 
 parser = argparse.ArgumentParser(description = "Things that OrthoFinder needs to run")
 parser.add_argument("dir", help = "path to a directory of fasta files that passed the busco threshold")
