45c1dfeba4b476aa5d5e1850a5374cc6ce05fd00
Jennifer L Hill <jenniferlhill@jennifers-air.aw4.unh.edu>
2018-02-21 10:08:58 -0500

    script to remove asterisks

--- pipeline_dev/remove_asterisks.py
+++ pipeline_dev/remove_asterisks.py
@@ -1,14 +1,26 @@
-#! usr/bin/env python3
+#! /usr/bin/env python3
 
-import re
+import argparse
 import os
 
+#function to take out the asterisks at the ends of protein sequences
 def asterisk():
     for entry in os.scandir("{0}".format(args.directory)):
-        try:
-            with open("{0}".format(entry.path)) as fasta_file:
-                for line in fasta_file:
-                    stop = re.search("\*", line)
-                    rem_stop = stop.replace("\*", "")
-        except FileNotFoundError:
-            print("file could not be found")
+        if entry.is_file():
+            try:
+                with open("{0}".format(entry.path), "r") as fasta_file:
+                    with open("temp_file.fasta", "w") as new_file:
+                        all_lines = ""
+                        for line in fasta_file:
+                            line_new = line.replace("*", "")
+                            all_lines += line_new
+                        new_file.write(all_lines)
+                    os.rename("temp_file.fasta", "{0}".format(entry.path))
+            except IOError:
+                print("problem reading file")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("directory", help = "path to a directory containing the protein fastas")
+args = parser.parse_args()
+
+asterisk()
