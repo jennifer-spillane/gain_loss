cecc67b127b3012766129a2064e0742eb2dab92b
Jennifer L Hill <jenniferlhill@jennifers-air.aw4.unh.edu>
2018-06-06 14:28:04 -0400

    updates

--- pipeline_dev/qual_cdhit_qual.py
+++ pipeline_dev/qual_cdhit_qual.py
@@ -25,6 +25,43 @@ def qual_cdhit_qual():
     subprocess.run(busco2, shell = True)
 
 
+def calc_busco():
+    #looping through all the busco scores for each species
+    for entry in os.scandir("{0}".format(args.orig_busco)):
+        score1 = None
+        score2 = None
+        score3 = None
+        whole = entry.name.split(".")
+        front = suffix[0].split("_")
+        #storing just the genus_species names for later
+        specname = "{0}_{1}".format(front[2], front[3])
+        print(specname)
+
+        #extracting the score from the original busco files
+        try:
+            with open("{0}".format(entry.name)) as first:
+                for line in first:
+                    line = line.strip()
+                    if line[0] == "C":
+                        score1 += float(line[2:6])
+
+            #extracting the score from the second busco run
+            with open("{0}/run_{1}_trans/short_summary_{1}_trans.txt".format(args.new_busco, specname)) as second:
+                for line in second:
+                    line = line.strip()
+                    if line[0] == "C":
+                        score2 += float(line[2:6])
+
+            #extracting the score from the third busco run
+            with open("{0}/run_{1}_cdhit/short_summary_{1}_cdhit.txt".format(args.final_busco, specname)) as third:
+                for line in third:
+                    line = line.strip()
+                    if line[0] == "C":
+                        score3 += float(line[2:6])
+
+            with open("{0}/specname.scores.txt", "w") as new_file:
+                
+
 parser = argparse.ArgumentParser(description = "Arguments for quality checking and filtering with cdhit")
 parser.add_argument("-i", "--infile", required = True, help = "absolute path to file to be filtered and buscoed")
 parser.add_argument("-s", "--similarity", type = float, default = 1.0, help = "similarity threshold for cdhit")
@@ -33,14 +70,12 @@ parser.add_argument("-f", "--final_busco", required = True, help = "path to dire
 parser.add_argument("-b", "--orig_busco", required = True, help = "path to the directory where original busco score is")
 parser.add_argument("-p", "--prefix", required = True, help = "prefix to go into the name of the busco output")
 parser.add_argument("-o", "--outfile", required = True, help = "path to the filtered assembly file")
+parser.add_argument("-bs", "--busco_scores", required = True, help = "path to where the busco score calculations should go")
 args = parser.parse_args()
 
 qual_cdhit_qual()
 
 #submitted at 1:50 or so.
-
-
-
 
 
 
--- pipeline_dev/reformat_headers.sh
+++ pipeline_dev/reformat_headers.sh
@@ -7,3 +7,67 @@ for file_name in *.pep
 do
     awk '/^>/{print ">" ++i; next}{print}' < ./$file_name > genus_species.fa
 done
+
+#! /usr/bin/env python3
+
+import os
+import subprocess
+
+for fasta in os. scandir("/mnt/lustre/macmaneslab/jlh1023/metazoa_matrix/batch1_filtered/"):
+    filename = fasta.name.split(".")
+    genespec = filename[0]
+
+
+awk '/^>/{$0=">Genus_species_"++i}1' input.fasta > output.fasta
+
+
+
+
+
+OG0381225: Gene.27787__lucernariopsis_campanulata_54495__g.27787__m.27787
+OG0356027: Gene.36946__hirondellea_gigas_87406__g.36946__m.36946
+OG0426389: Gene.53874__pleraplysilla_spinifera_103554__g.53874__m.53874
+OG0440104: Gene.23036__proasellus_beticus_110697__g.23036__m.23036
+OG0339498: Gene.14041__hemithiris_psittacea_16303__g.14041__m.14041
+OG0144664: Homo_sapiens_47396
+OG0033621: Caenorhabditis_elegans_12948 Caenorhabditis_elegans_12949 Meloidogyne_incognita_12895
+OG0129370: Drosophila_melanogaster_22009
+OG0276587: Gene.11977__clione_antarctica_24043__g.11977__m.11977
+OG0025577: Capitella_teleta_20342 Gene.1889__convolutriloba_macropyga_2780__g.1889__m.1889 Gene.60737__pleraplysilla_spinifera_114915__g.60737__m.60737 Ixodes_scapularis_13782 Nematostella_vectensis_14569
+
+#! /usr/bin/env python3
+
+try:
+    with open("{0}".format(args.in) "r") as infile:
+        for line in infile:
+            line = line.split("\t")
+            
+
+
+import argparse
+import re
+import Bio.SeqIO
+
+parser = argparse.ArgumentParser(description = "arguments for changing headers")
+parser.add_argument("-p", "--pasta", required = True, help = "input ali file to change names of")
+parser.add_argument("-o", "--out", required = True, help = "name of out file")
+args = parser.parse_args()
+
+try:
+    all_lines = []
+    for record in Bio.SeqIO.parse("{0}".format(args.pasta), "fasta"):
+        header = re.search("([a-z]+)([0-9]*)", record.id)
+        species = header.group(1)
+        acc = header.group(2)
+        cap_species = species.capitalize()
+        new_hydra = ""
+        if cap_species.startswith("Hydra"):
+            hydra_cap_species = cap_species[0:5]
+            remaining = cap_species[5:]
+            new_hydra = "{0}_{1}".format(hydra_cap_species, remaining)
+        new_header = "{0}_{1}".format(new_hydra, acc) if len(new_hydra) > 0 else "{0}_{1}".format(cap_species, acc)
+        current_record = Bio.SeqRecord.SeqRecord(id = new_header, seq = record.seq, description = "")
+        all_lines.append(current_record)
+    Bio.SeqIO.write(all_lines, "{0}".format(args.out), "fasta")
+except IOError:
+print("Ya dumb!")
