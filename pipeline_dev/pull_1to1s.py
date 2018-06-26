e1adc79a6236923af8835f776c4c3f5ed6d66766
Jennifer L Hill <jenniferlhill@jennifers-air.aw4.unh.edu>
2018-06-26 15:10:20 -0400

    fixed efficiency and messages

--- pipeline_dev/pull_1to1s.py
+++ pipeline_dev/pull_1to1s.py
@@ -6,25 +6,41 @@ import Bio.SeqIO
 #function to pull members of one-to-one orthogroups from a protein fasta file
 def pull():
     try:
+        #creating an empty set and dictionary to hold orthogroups
         ogs = set()
         ols = {}
+        prot_set = set()
         with open("{}".format(args.cluster), "r") as cluster_file:
             with open("{}".format(args.ortho), "r") as ortho_file:
+                #saving all the orthogroup names in a set
+                print("Getting orthogroup names from kinfin file")
                 for line in cluster_file:
                     line = line.split("\t")
-                    if line.startswith("OG"):
+                    if line[0].startswith("OG"):
                         ogs.add(line[0])
+                print("Pulled orthogroup names from kinfin file")
+                #populating the dictionary with keys = orthogroup names,
+                #and values = a list of the proteins in that orthogroup
+                #also making a set that contains all the protein names
+                print("Getting protein names from orthofinder file")
                 for line in ortho_file:
+                    line = line.rstrip()
                     line = line.split("\t")
+
                     if line[0] in ogs:
                         ols.setdefault(line[0], line[1:])
-
+                        for protein in line[1:]:
+                            protein = protein.split(", ")
+                            for indv in protein:
+                                prot_set.add(indv)
+                print("Pulled protein names from orthofinder file")
+        print("Parsing the fasta file")
         prot_seqs = []
         for record in Bio.SeqIO.parse("{}".format(args.prots), "fasta"):
-            for item in ols[item]:
-                if record.id == ols[item]:
-                    cur_prot = Bio.SeqRecord.SeqRecord(id = record.id, seq = record.seq)
-                    prot_seqs.append(cur_prot)
+            if record.id in prot_set:
+                cur_prot = Bio.SeqRecord.SeqRecord(id = record.id, seq = record.seq)
+                prot_seqs.append(cur_prot)
+        print("Writing new fasta file")
         Bio.SeqIO.write(prot_seqs, "{}".format(args.out), "fasta")
 
     except IOError:
@@ -32,8 +48,9 @@ def pull():
 
 parser = argparse.ArgumentParser(description = "arguments for pulling 1-to-1 orthologues from a fasta")
 parser.add_argument("-c", "--cluster", required = True, help = "all.all.cluster_1to1s.txt provided by kinfin")
-parser.add_argument("-o", "--ortho", required = True, help = "Orthologues.csv provided by orthofinder")
+parser.add_argument("-r", "--ortho", required = True, help = "Orthogroups.csv provided by orthofinder")
 parser.add_argument("-p", "--prots", required = True, help = "fasta file containing all proteins in the orthofinder analysis")
 parser.add_argument("-o", "--out", required = True, help = "name of the output fasta file")
+args = parser.parse_args()
 
 pull()
